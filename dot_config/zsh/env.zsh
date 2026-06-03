b6a5bc41ea8481a3c14c697b82590a905fd63957
Calvin <hello@calvindass.com>
2026-06-03 10:15:25 +1000

    fix(zsh): template env and secrets

--- dot_config/zsh/env.zsh
+++
@@ -1,9 +0,0 @@
-# Added by ForgeCode installer
-export PATH="/Users/oakypokey/.local/bin:$PATH"
-export PATH="$PATH:/Users/oakypokey/Bin"
-export PATH="$PATH:/Users/oakypokey/.local/bin:/usr/local/bin:/Users/oakypokey/.dotnet/tools"
-
-export EDITOR="nvim"
-export GITEA_HOST="https://cryptic.spren"
-
-AUTO_NOTIFY_IGNORE+=("docker" "cde")
--- dot_config/zsh/secrets.zsh
+++
@@ -1,1 +0,0 @@
-export GITEA_ACCESS_TOKEN="$(security find-generic-password -s "GITEA_ACCESS_TOKEN" -w -a "$(whoami)")"
