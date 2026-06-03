5070115efd9a9307911cd68cfe2d3135b1b2b11d
Calvin <hello@calvindass.com>
2026-06-04 09:30:08 +1000

    feat(wezterm): enable terminal config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,88 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+local kanagawa = wezterm.plugin.require("https://github.com/sravioli/kanagawa.wz")
+local cmdpicker = wezterm.plugin.require("https://github.com/abidibo/wezterm-cmdpicker")
+
+kanagawa.apply_to_config(config, {
+	scheme = "wave",
+})
+
+config.font = wezterm.font_with_fallback({
+	{
+		family = "JetBrainsMono Nerd Font",
+		harfbuzz_features = {
+			"calt=1",
+			"clig=1",
+			"liga=1",
+		},
+	},
+})
+
+config.font_size = 14.0
+config.line_height = 1.05
+
+-- Slight transparency
+config.window_background_opacity = 0.92
+config.macos_window_background_blur = 20
+
+-- Neovim-friendly
+config.term = "xterm-256color"
+config.enable_kitty_keyboard = true
+config.enable_csi_u_key_encoding = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 10000
+config.adjust_window_size_when_changing_font_size = false
+
+-- Leader key
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+	{ key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
+	-- Pane navigation that plays nicely with neovim/tmux
+	{ key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+
+	-- Splits
+	{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- Tabs
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+
+	-- Opencode
+	{
+		key = "O",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(window, pane)
+			local right = pane:split({
+				direction = "Right",
+				size = 0.25,
+			})
+
+			local top = right:split({
+				direction = "Top",
+				size = 0.5,
+				args = { "/bin/zsh", "-i" },
+			})
+
+			top:send_text("opencode --port\n")
+		end),
+	},
+}
+
+cmdpicker.apply_to_config(config, {
+	key = " ",
+	mods = "LEADER",
+	title = "Command Palette",
+	include_defaults = true,
+	fuzzy = true,
+})
+
+return config
