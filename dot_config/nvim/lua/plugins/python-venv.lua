b2f1efc6275053fa29a7230d667bdf65d1a9ee28
Calvin <hello@calvindass.com>
2026-06-03 14:22:52 +1000

    feat(nvim): detect Python virtualenvs

---
+++ dot_config/nvim/lua/plugins/python-venv.lua
@@ -0,0 +1,24 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		vim.pack.add({
+			"https://github.com/tnfru/nvim-venv-detector",
+		})
+
+		require("venv_detector").setup({
+			auto_activate_venv = true,
+			auto_restart_lsp = true,
+
+			lsp_client_names = {
+				"pyright",
+				"pylsp",
+				"ruff",
+				"ruff_lsp",
+				"basedpyright",
+				"python",
+			},
+
+			notify = true,
+		})
+	end,
+})
