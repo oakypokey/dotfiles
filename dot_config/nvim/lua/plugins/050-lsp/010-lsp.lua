89543483a618a530ea91e54932fb13fe16c2d65e
Calvin <hello@calvindass.com>
2026-06-04 12:27:18 +1000

    fix(nvim): refine LSP tooling

--- dot_config/nvim/lua/plugins/050-lsp/010-lsp.lua
+++ dot_config/nvim/lua/plugins/050-lsp/010-lsp.lua
@@ -1,4 +1,4 @@
-local gh = require 'util.github'
+local gh = require("util.github")
 
 -- ============================================================
 -- SECTION 5: LSP
@@ -33,14 +33,18 @@ local gh = require 'util.github'
 
 -- Useful status updates for LSP.
 local specs = {
-  {
-    src = gh('j-hui/fidget.nvim'),
-    config = function() require('fidget').setup {} end,
-  },
-  {
-    src = gh('mason-org/mason.nvim'),
-    config = function() require('mason').setup {} end,
-  },
+	{
+		src = gh("j-hui/fidget.nvim"),
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
+	{
+		src = gh("mason-org/mason.nvim"),
+		config = function()
+			require("mason").setup({})
+		end,
+	},
 }
 
 -- Enable the following language servers
@@ -48,81 +52,104 @@ local specs = {
 --  See `:help lsp-config` for information about keys and how to configure
 ---@type table<string, vim.lsp.Config>
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`ts_ls`) will work just fine
-  ts_ls = {},
+	-- clangd = {},
+	-- gopls = {},
+	-- pyright = {},
+	-- rust_analyzer = {},
+	--
+	-- Some languages (like typescript) have entire language plugins that can be useful:
+	--    https://github.com/pmizio/typescript-tools.nvim
+	--
+	-- But for many setups, the LSP (`ts_ls`) will work just fine
+	ts_ls = {
+		root_dir = function(bufnr, on_dir)
+			on_dir(vim.fs.root(bufnr, {
+				"tsconfig.json",
+				"jsconfig.json",
+				"deno.json",
+				"deno.jsonc",
+				"deno.lock",
+				"package.json",
+				"package-lock.json",
+				"yarn.lock",
+				"pnpm-lock.yaml",
+				"bun.lock",
+				"bun.lockb",
+				".git",
+			}) or vim.fn.getcwd())
+		end,
+	},
 
-  stylua = {}, -- Used to format Lua code
+	stylua = {}, -- Used to format Lua code
 
-  -- Special Lua Config, as recommended by neovim help docs
-  lua_ls = {
-    on_init = function(client)
-      client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
+	-- Special Lua Config, as recommended by neovim help docs
+	lua_ls = {
+		on_init = function(client)
+			client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
 
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-      end
+			if client.workspace_folders then
+				local path = client.workspace_folders[1].name
+				if
+					path ~= vim.fn.stdpath("config")
+					and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+				then
+					return
+				end
+			end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          version = 'LuaJIT',
-          path = { 'lua/?.lua', 'lua/?/init.lua' },
-        },
-        workspace = {
-          checkThirdParty = false,
-          -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-          --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-          library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-            '${3rd}/luv/library',
-            '${3rd}/busted/library',
-          }),
-        },
-      })
-    end,
-    ---@type lspconfig.settings.lua_ls
-    settings = {
-      Lua = {
-        format = { enable = false }, -- Disable formatting (formatting is done by stylua)
-      },
-    },
-  },
+			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+				runtime = {
+					version = "LuaJIT",
+					path = { "lua/?.lua", "lua/?/init.lua" },
+				},
+				workspace = {
+					checkThirdParty = false,
+					-- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+					--  See https://github.com/neovim/nvim-lspconfig/issues/3189
+					library = vim.tbl_extend("force", vim.api.nvim_get_runtime_file("", true), {
+						"${3rd}/luv/library",
+						"${3rd}/busted/library",
+					}),
+				},
+			})
+		end,
+		---@type lspconfig.settings.lua_ls
+		settings = {
+			Lua = {
+				format = { enable = false }, -- Disable formatting (formatting is done by stylua)
+			},
+		},
+	},
 }
 
 table.insert(specs, {
-  src = gh('neovim/nvim-lspconfig'),
-  dependencies = {
-    { src = gh('mason-org/mason-lspconfig.nvim') },
-    { src = gh('WhoIsSethDaniel/mason-tool-installer.nvim') },
-  },
-  config = function()
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      -- You can add other tools here that you want Mason to install
-      'ruff',
-    })
+	src = gh("neovim/nvim-lspconfig"),
+	dependencies = {
+		{ src = gh("mason-org/mason-lspconfig.nvim") },
+		{ src = gh("WhoIsSethDaniel/mason-tool-installer.nvim") },
+	},
+	config = function()
+		-- Ensure the servers and tools above are installed
+		--
+		-- To check the current status of installed tools and/or manually install
+		-- other tools, you can run
+		--    :Mason
+		--
+		-- You can press `g?` for help in this menu.
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			-- You can add other tools here that you want Mason to install
+			"ruff",
+			"markdownlint",
+		})
 
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-    for name, server in pairs(servers) do
-      vim.lsp.config(name, server)
-      vim.lsp.enable(name)
-    end
-  end,
+		for name, server in pairs(servers) do
+			vim.lsp.config(name, server)
+			vim.lsp.enable(name)
+		end
+	end,
 })
 
 return specs
