a10a568e34676bc2dc349bb58273ad18c766973d
Calvin <hello@calvindass.com>
2026-06-22 11:21:40 +1000

    feat(nvim): use snacks explorer

--- dot_config/nvim/lua/plugins/ui/snacks.lua
+++ dot_config/nvim/lua/plugins/ui/snacks.lua
@@ -1,10 +1,18 @@
 local repo = require 'tooling.repos'
 
 return repo.spec('snacks', {
+  priority = 0,
   config = function()
+    --@type snacks.Config
     require('snacks').setup {
+      dashboard = { sections = {
+        { section = 'header' },
+        { section = 'keys' },
+      } },
       input = {},
       picker = {},
+      explorer = {},
+      scope = {},
     }
   end,
 })
