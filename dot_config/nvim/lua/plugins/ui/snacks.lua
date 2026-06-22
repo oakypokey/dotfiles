03b25cd0de79264b129ab83423cd56b81461b9dd
Calvin <hello@calvindass.com>
2026-06-22 17:25:15 +1000

    feat(explorer): make this toggle-able

--- dot_config/nvim/lua/plugins/ui/snacks.lua
+++ dot_config/nvim/lua/plugins/ui/snacks.lua
@@ -4,15 +4,31 @@ return repo.spec('snacks', {
   priority = 0,
   config = function()
     --@type snacks.Config
-    require('snacks').setup {
+    local Snacks = require 'snacks'
+    Snacks.setup {
       dashboard = { sections = {
         { section = 'header' },
         { section = 'keys' },
       } },
       input = {},
-      picker = {},
+      picker = {
+        sources = {},
+      },
       explorer = {},
       scope = {},
     }
+
+    local function toggle_explorer()
+      local explorer_win = Snacks.picker.get({ source = 'explorer' })[1]
+      if explorer_win == nil then
+        Snacks.picker.explorer()
+      elseif explorer_win:is_focused() then
+        Snacks.picker.explorer()
+      else
+        vim.cmd 'wincmd p'
+      end
+    end
+
+    vim.keymap.set('n', '\\', toggle_explorer, { desc = 'Toggle/Jump to Snacks Explorer' })
   end,
 })
